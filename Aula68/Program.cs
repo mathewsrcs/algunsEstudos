f95b2a46bc954c9dbfc78e74956f589123a9b3d5
matheussimione <matheus.simione@grupobarigui.com.br>
2026-03-13 16:37:24 -0300

    terminar em casa

--- Aula68/Program.cs
+++ Aula68/Program.cs
@@ -9,12 +9,15 @@ Console.WriteLine("-------------------------------------------");
 Carro c2 = new("SUV", "Ford", "EcoSport", 2018, 120);
 Console.WriteLine($"{c2.Modelo}, {c2.Montadora}, {c2.Marca}, {c2.Ano}, {c2.Potencia}");
 c2.Acelerar();
+Console.WriteLine($"Potência antes: {c2.Potencia}");
+int novaPotencia = c2.AumentarPotencia(c2.Potencia);
+Console.WriteLine($"Retorno do método: {novaPotencia}");
+Console.WriteLine($"Potência de c2 após a chamada: {c2.Potencia}");
 
 Console.WriteLine("-------------------------------------------");
 
 Carro c3 = new Carro("Hatch", "Renault");
 Console.WriteLine($"{c3.Modelo}, {c3.Montadora}");
-
 Console.ReadKey();
 
 public class Carro
@@ -39,5 +42,18 @@ public class Carro
     public void Acelerar()
     {
         Console.WriteLine($"Acelerando meu {Marca}...");
+    }
+    public double VelocidadeMaxima(int potencia)
+    {
+        double velocidadeMaxima  = potencia * 1.75;
+        return velocidadeMaxima;
+    }
+    public int AumentarPotencia (int potencia)
+    {
+        return potencia + 3;
+    }
+    public int AumentarPotencia (int potencia)
+    {
+        return potencia + 5; // terminar esse trem em casa
     }
 }
